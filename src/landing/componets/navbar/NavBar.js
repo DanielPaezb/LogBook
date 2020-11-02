de47cfb3b435a91860be2a6679b1b2cee55f2aae
Nicolas-alt <a10100105555@gmail.com>
2020-11-02 17:39:35 -0500

    add(component) NavBar

---
+++ src/landing/componets/navbar/NavBar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const NavBar = _ => {
+    return(
+      <header>
+          <nav>
+              <ul>
+                <li><a >LogIn</a></li>
+                <li><a >SingIn</a></li>
+                <li><a ></a>About</li>
+                <li><a ></a>Team</li>
+                <li><a >Terminos y condiciones</a></li>
+              </ul>
+          </nav>
+      </header>
+    );
+}
+
+export default NavBar
