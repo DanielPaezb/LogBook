cadd3804c3653256b16b6c4d5a79bb01377806ae
DanielPaezb <dcpaez595@misena.edu.co>
2020-11-03 15:33:00 -0500

    feat() Se agrega el siguiente componente:s  *Se corrige un poco la indentación de los siguientes componentes:       ->Home.js       ->notFound404  *Se agrega la hoja de estilos .scss [vacía] del componente:       ->DashBoard.js  *Se agrega el siguiente componente al proyecto:       ->Footer.js       ->footer.scss

--- src/landing/home/Home.js
+++ src/landing/home/Home.js
@@ -1,28 +1,33 @@
-import React from 'react'
-import img from '../../assets/svg/landing.svg';
-import NavBar from '../componets/navbar/NavBar';
-import './home.scss';
+import React from "react";
+import img from "../../assets/svg/landing.svg";
+import NavBar from "../componets/navbar/NavBar";
+import "./home.scss";
 
 export const Home = () => {
-    return (
-        <>
-            <NavBar />
-            <section className="section--container">
-                <section className="section--midle section--midle1">
-                    <h1>LogBook</h1>
-                    <p className="p--lema">Gestiona el mundo en un solo click.</p>
-                    <p className="p--informacion">
-                        LogBook es un modulo que permite administrar personal de forma sencilla y práctica, haciendo uso
-                        de herramientas he ingenio para obtener el mayor rendimiento en sus propositos.
-        </p>
-                </section>
+  return (
+    <>
+      <NavBar />
+      <section className="section--container">
+        <section className="section--midle section--midle1">
+          <h1>LogBook</h1>
+          <p className="p--lema">Gestiona el mundo en un solo click.</p>
+          <p className="p--informacion">
+            LogBook es un modulo que permite administrar personal de forma
+            sencilla y práctica, haciendo uso de herramientas he ingenio para
+            obtener el mayor rendimiento en sus propositos.
+          </p>
+        </section>
 
-                <section className="section--midle section--midle2">
-                    <img className="img--svg" src={ img } alt="Imagen de personal gestionando en celular." />
-                </section>
-            </section>
-        </>
-    );
-}
+        <section className="section--midle section--midle2">
+          <img
+            className="img--svg"
+            src={img}
+            alt="Imagen de personal gestionando en celular."
+          />
+        </section>
+      </section>
+    </>
+  );
+};
 
 export default Home;
