345dae66de2409f2579825b0aeda6ebd961d319d
Nicolas-alt <a10100105555@gmail.com>
2020-11-05 11:03:29 -0500

    update(styles)

--- src/landing/componets/navbar/NavBar.js
+++ src/landing/componets/navbar/NavBar.js
@@ -1,31 +1,31 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import './navbar.scss';
 
 export const NavBar = () => {
-    return (
-        <header>
-            <nav>
-                <ul>
-                    <li>
-                        <NavLink to="/" >Home</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/team" >Team</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/terms" >Terms</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/login" >SignIn</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/register" >SignUp</NavLink>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-    );
+  return (
+    <header className="header--nav">
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" >Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/team" >Team</NavLink>
+          </li>
+          <li>
+            <NavLink to="/terms" >Terms</NavLink>
+          </li>
+          <li>
+            <NavLink to="/login" >SignIn</NavLink>
+          </li>
+          <li>
+            <NavLink to="/register" >SignUp</NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default NavBar;
--- src/landing/home/Home.js
+++ src/landing/home/Home.js
@@ -1,28 +1,25 @@
 import React from 'react'
 import img from '../../assets/svg/landing.svg';
-import NavBar from '../componets/navbar/NavBar';
 import './home.scss';
 
 export const Home = () => {
-    return (
-        <>
-            <NavBar />
-            <section className="section--container">
-                <section className="section--midle section--midle1">
-                    <h1>LogBook</h1>
-                    <p className="p--lema">Gestiona el mundo en un solo click.</p>
-                    <p className="p--informacion">
-                        LogBook es un modulo que permite administrar personal de forma sencilla y práctica, haciendo uso
-                        de herramientas he ingenio para obtener el mayor rendimiento en sus propositos.
-        </p>
-                </section>
-
-                <section className="section--midle section--midle2">
-                    <img className="img--svg" src={ img } alt="Imagen de personal gestionando en celular." />
-                </section>
-            </section>
-        </>
-    );
+  return (
+    <>
+      <section className="section--main">
+        <section className="containerItem section--mainItem1">
+          <h1>LogBook</h1>
+          <p className="p--lema">Gestiona el mundo en un solo click.</p>
+          <p className="p--informacion">
+            LogBook es un modulo que permite administrar personal de forma sencilla y práctica, haciendo uso
+            de herramientas he ingenio para obtener el mayor rendimiento en sus propositos.
+                    </p>
+        </section>
+        <section className="containerItem section--mainItem2">
+          <img className="img--svg" src={img} alt="Imagen de personal gestionando en celular." />
+        </section>
+      </section>
+    </>
+  );
 }
 
 export default Home;
