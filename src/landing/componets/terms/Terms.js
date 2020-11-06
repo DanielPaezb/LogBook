49cab6c5d47ef648cff4b15563b08521ccd41c97
Nicolas-alt <a10100105555@gmail.com>
2020-11-06 09:13:21 -0500

    update(termsStyles)

--- src/landing/componets/terms/Terms.js
+++ src/landing/componets/terms/Terms.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import './terms.scss'
 
 export const Terms = () => {
   return (
-        <div className="jumbotron jumbotron-fluid">
-          <div className="container">
-            <h1 className="display-4">POLÍTICA DE PRIVACIDAD</h1>
-            <p className="lead">
+        <section className="section--terms containerItem">
+            <h1 className="h1--terms">POLÍTICA DE PRIVACIDAD</h1>
+            <p className="p--terms">
               El presente Política de Privacidad establece los términos en que LogBook usa y protege la información que es proporcionada por sus usuarios
               al momento de utilizar su sitio web.
               Esta compañía está comprometida con la seguridad de los datos de sus usuarios. Cuando le pedimos llenar los campos de información personal 
@@ -21,21 +21,21 @@ export const Terms = () => {
               LogBook está altamente comprometido para cumplir con el compromiso de mantener su información segura. Usamos los sistemas más avanzados y los actualizamos constantemente para asegurarnos que no exista ningún acceso no autorizado.
             </p>
 
-            <h3 className="display-4">
+            <h3 className="h3--terms">
               Cookies
             </h3>
               
-            <p className="lead">
+            <p className="p--terms">
               Una cookie se refiere a un fichero que es enviado con la finalidad de solicitar permiso para almacenarse en su ordenador,
               al aceptar dicho fichero se crea y la cookie sirve entonces para tener información respecto al tráfico web, y también facilita las futuras recurrente. Otra función que tienen las cookies es que con ellas las web pueden reconocerte individualmente y por tanto brindarte el mejor servicio personalizado de su web.
               
-              Nuestro sitio web emplea las cookies para poder identificar las páginas que son visitadas y su frecuencia. Esta información es empleada únicamente para análisis estadístico y después la información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, estás no dan acceso a información de su ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente. Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores aceptan cookies automáticamente pues sirve para tener un mejor servicio web. También usted puede cambiar la configuración de su ordenador para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.
+              Nuestro sitio web emplea las cookies para poder identificar las páginas que son visitadas y su frecuencia. Esta información es empp--termsa únicamente para análisis estadístico y después la información se elimina de forma permanente. Usted puede eliminar las cookies en cualquier momento desde su ordenador. Sin embargo las cookies ayudan a proporcionar un mejor servicio de los sitios web, estás no dan acceso a información de su ordenador ni de usted, a menos de que usted así lo quiera y la proporcione directamente. Usted puede aceptar o negar el uso de cookies, sin embargo la mayoría de navegadores aceptan cookies automáticamente pues sirve para tener un mejor servicio web. También usted puede cambiar la configuración de su ordenador para declinar las cookies. Si se declinan es posible que no pueda utilizar algunos de nuestros servicios.
             </p>
             
-            <h3 className="display-4">
+            <h3 className="h3--terms">
               Enlaces a Terceros
             </h3>
-            <p className="lead">
+            <p className="p--terms">
               Este sitio web pudiera contener en laces a otros sitios que pudieran ser de su interés. Una vez que usted de clic en estos enlaces y abandone nuestra página, ya no tenemos control sobre al sitio al que es redirigido y por lo tanto no somos responsables de los términos o privacidad ni de la protección de sus datos en esos otros sitios terceros. Dichos sitios están sujetos a sus propias políticas de privacidad por lo cual es recomendable que los consulte para confirmar que usted está de acuerdo con estas.
               
               Control de su información personal
@@ -46,8 +46,7 @@ export const Terms = () => {
               
               LogBook Se reserva el derecho de cambiar los términos de la presente Política de Privacidad en cualquier momento.
             </p>
-          </div>
-      </div>
+      </section>
   );
 }
 
