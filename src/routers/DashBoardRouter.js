c59e83037de3058aecf96f961a75b1df068400c4
Nicolas-alt <a10100105555@gmail.com>
2020-12-02 21:59:05 -0500

    chore(tabs)

--- src/routers/DashBoardRouter.js
+++ src/routers/DashBoardRouter.js
@@ -1,4 +1,5 @@
-import {  Redirect, Route, Switch } from 'react-router-dom';
+import {Redirect, Route, Switch} from 'react-router-dom';
+
 import Error from '../views/dashboard/components/error/Error';
 import Filter from '../views/dashboard/components/filter/Filter';
 import Home from '../views/dashboard/components/home/Home';
@@ -11,23 +12,24 @@ import Profile from '../views/dashboard/components/profile/Profile';
 import SearchScreen from '../views/dashboard/components/search/SearchScreen';
 import Settings from '../views/dashboard/components/settings/Settings';
 
-export const DashBoardRouter= () => {
-    return (
-            <Switch>
-                <Route exact path="/dash/home" component={Home} />
-                <Route exact path="/dash/seach" component={SearchScreen} />
-                <Route exact path="/dash/settings" component={Settings} />
-                <Route exact path="/dash/profile" component={Profile} />
-                <Route exact path="/dash/filter" component={Filter} />
-                <Route exact path="/dash/log" component={ActivityLog} />
-                <Route exact path="/dash/notifications" component={NotificationScreen} />
-                <Route exact path="/dash/messages" component={MessageScreen} />
-                <Route exact path="/dash/add" component={NewUser} />
-                <Route exact path="/dash/error" component={Error} />
-                <Route exact path="/dash/feelback" component={Opinion} />
-                <Redirect to="/dash/home" />
-            </Switch>
-    );
+export const DashBoardRouter = () => {
+  return (
+    <Switch>
+      <Route exact path="/dash/home" component={Home} />
+      <Route exact path="/dash/seach" component={SearchScreen} />
+      <Route exact path="/dash/settings" component={Settings} />
+      <Route exact path="/dash/profile" component={Profile} />
+      <Route exact path="/dash/filter" component={Filter} />
+      <Route exact path="/dash/log" component={ActivityLog} />
+      <Route exact path="/dash/notifications" component={NotificationScreen} />
+      <Route exact path="/dash/messages" component={MessageScreen} />
+      <Route exact path="/dash/add" component={NewUser} />
+      <Route exact path="/dash/error" component={Error} />
+      <Route exact path="/dash/feelback" component={Opinion} />
+      <Redirect to="/dash/home" />
+    </Switch>
+  );
 }
 
 export default DashBoardRouter;
+
--- src/views/dashboard/components/ui/sidebar/Sidebar.js
+++ src/views/dashboard/components/ui/sidebar/Sidebar.js
@@ -1,60 +1,69 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 
 const Sidebar = () => {
-    return (
-             <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
-             <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/dash/home">
-                 <div className="sidebar-brand-icon rotate-n-15">
-                     <i className="fas fa-laugh-wink"></i>
-                 </div>
-                 <div className="sidebar-brand-text mx-3">LogBook</div>
-             </Link>
- 
-             <hr className="sidebar-divider" />
-             
-             <div className="sidebar-heading">
-                 General
-             </div>
-             
-             <li className="nav-item">
-                 <Link className="nav-link" to="/dash/home" >
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Home</span>
-                </Link>
-             </li>
+  return (
+    <ul className="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
 
-             <li className="nav-item">
-                 <Link className="nav-link" to="/dash/filter" >
-                 <i className="fas fa-fw fa-cog"></i>
-                    <span>Filter</span>
-                </Link>
-             </li>
+      <Link className="sidebar-brand d-flex align-items-center justify-content-center" to="/dash/home">
+        <div className="sidebar-brand-icon rotate-n-15">
+          <i className="fas fa-laugh-wink"></i>
+        </div>
+        <div className="sidebar-brand-text mx-3">LogBook</div>
+      </Link>
 
-           
-             <li className="nav-item">
-                 <Link className="nav-link" to="/dash/add" >
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>New People</span>
-                </Link>
-             </li>
+      <hr className="sidebar-divider" />
 
-             <li className="nav-item">
-                 <Link className="nav-link" to="/dash/error" >
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Error Report</span>
-                </Link>
-             </li>
+      <div className="sidebar-heading">
+        General
+      </div>
 
-             <li className="nav-item">
-                 <Link className="nav-link" to="/dash/feelback" >
-                    <i class="fas fa-fw fa-table"></i>
-                    <span>Feelback</span>
-                </Link>
-             </li>
+      <li className="nav-item">
+        <Link className="nav-link" to="/dash/home" >
+          <i class="fas fa-fw fa-table"></i>
+          <span>Home</span>
+        </Link>
+      </li>
 
-         </ul>
-    )
+      <li className="nav-item">
+        <Link className="nav-link" to="/dash/filter" >
+          <i className="fas fa-fw fa-cog"></i>
+          <span>Filter</span>
+        </Link>
+      </li>
+
+
+      <li className="nav-item">
+        <Link className="nav-link" to="/dash/add" >
+          <i class="fas fa-fw fa-table"></i>
+          <span>New People</span>
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" to="/dash/error" >
+          <i class="fas fa-fw fa-table"></i>
+          <span>Error Report</span>
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" to="/dash/feelback" >
+          <i class="fas fa-fw fa-table"></i>
+          <span>Feelback</span>
+        </Link>
+      </li>
+
+      <li className="nav-item">
+        <Link className="nav-link" to="/dash/feelback" >
+          <i class="fas fa-fw fa-table"></i>
+          <span>Feelback</span>
+        </Link>
+      </li>
+
+
+    </ul>
+  )
 }
 
 export default Sidebar;
